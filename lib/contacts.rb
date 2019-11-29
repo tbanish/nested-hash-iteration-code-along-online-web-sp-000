f86efa1c1118ba85cd817788641ea696b42b156e
Tom Banish <tombanish@gmail.com>
2019-11-29 14:42:47 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,6 +18,8 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
-    binding.pry
+        binding.pry
+      end
+    end  
   end
 end
